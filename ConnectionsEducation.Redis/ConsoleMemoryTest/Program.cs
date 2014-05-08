e38714e81554305e2a190c2490af727739113aea
Maxwell Bloch <mbloch@connectionseducation.com>
2014-05-08 00:00:12 -0400

    ConsoleMemoryTest - add formatting to output

--- ConnectionsEducation.Redis/ConsoleMemoryTest/Program.cs
+++ ConnectionsEducation.Redis/ConsoleMemoryTest/Program.cs
@@ -1,13 +1,18 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace ConsoleMemoryTest {
 	class Program {
 		static void Main(string[] args) {
-			DateTime begin = DateTime.Now;
+			Stopwatch stopwatch = new Stopwatch();
+			const int ITERS = 100;
+			const int BATCH = 1000;
+			double lastMs = 0.0;
 
-			while (DateTime.Now < begin.AddMinutes(5)) {
+			stopwatch.Start();
+			for (int iter = 1; iter <= ITERS; ++iter) {
 				int success = 0;
-				for (int i = 0; i < 1000; ++i) {
+				for (int i = 0; i < BATCH; ++i) {
 					string guid = Guid.NewGuid().ToString("d");
 					var conn = new ConnectionsEducation.Redis.Connection();
 					conn.set("foo" + i, guid);
@@ -16,10 +21,19 @@ namespace ConsoleMemoryTest {
 						success++;
 				}
 
-				Console.WriteLine("{0}: {1}", DateTime.Now.Subtract(begin).TotalSeconds, success);
+				Console.Write("[{0:mm\\:ss\\.f}]", stopwatch.Elapsed);
+				Console.Write(success == BATCH ? "+  " : "-  ");
+				if (lastMs > 0.01)
+					Console.Write("{0:0.00}ops/s, ", 1000.0 * BATCH / (stopwatch.ElapsedMilliseconds - lastMs));
+				lastMs = stopwatch.ElapsedMilliseconds;
+				double rateAvg = 1000.0 * iter * BATCH / lastMs;
+				Console.WriteLine("Avg: {0:0.00}ops/s", rateAvg);
 			}
+			stopwatch.Stop();
 
 			Console.WriteLine();
+			Console.Write("[{0:mm\\:ss\\.f}]", stopwatch.Elapsed);
+			Console.WriteLine("   Total Avg: {0:0.00}ops/s", 1000.0 * ITERS * BATCH / lastMs);
 			Console.WriteLine("Done.");
 			Console.ReadLine();
 		}
